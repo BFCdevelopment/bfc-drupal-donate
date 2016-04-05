939d0c36c52b5de0e9bf01e85d04a3ecb8cde7ce
Arielle <Yellie@Arielles-MacBook-Pro.local>
2016-04-05 19:42:06 -0400

    Custom Donate / Stripe Module updates
    created config page, integrated with custom js file and php from rule

--- bfc_drupal_donate.js
+++ bfc_drupal_donate.js
@@ -1,40 +1,50 @@
-// This identifies your website in the createToken call below
-Stripe.setPublishableKey('pk_live_Vxr7cQ4jrsgH8pUJQJt081pA');
+(function ($) {
+  Drupal.behaviors.bfc_drupal_donate = {
+    attach: function (context, settings) {
+     // This identifies your website in the createToken call below
 
-jQuery(function($) {
-  $('#webform-client-form-1').submit(function(event) {
-    var $form = $(this);
+     //alert(Drupal.settings.bfc_drupal_donate.custom_form_id);
+     var pubkey = Drupal.settings.bfc_drupal_donate.stripe_publishable_key;
+     var formID = Drupal.settings.bfc_drupal_donate.custom_form_id;
+     Stripe.setPublishableKey(pubkey);
 
-    // Disable the submit button to prevent repeated clicks
-    $form.find('input.form-submit').prop('disabled', true).css("background-color","gray").val("Please wait...");
+      jQuery(function($) {
+        $('#webform-client-form-' + formID).submit(function(event) {
+          var $form = $(this);
 
-    Stripe.card.createToken($form, stripeResponseHandler);
+          // Disable the submit button to prevent repeated clicks
+          $form.find('input.form-submit').prop('disabled', true).css("background-color","gray").val("Please wait...");
 
-    // Prevent the form from submitting with the default action
-    return false;
-  });
-});
+          Stripe.card.createToken($form, stripeResponseHandler);
 
-function stripeResponseHandler(status, response) {
-  var $form = jQuery('#webform-client-form-1');
+          // Prevent the form from submitting with the default action
+          return false;
+        });
+      });
 
-  if (response.error) {
-    // Show the errors on the form
-    $form.find('.webform-component--error-markup').text(response.error.message);
-    $form.find('.form-submit').prop('disabled', false).css("background-color","").val("Make Payment");
-  } else {
-    // response contains id and card, which contains additional card details
-    var token = response.id;
-    // Insert the token into the form so it gets submitted to the server
-    $form.find('.webform-component--stripe-token input').val(token);
-    
-    // Remove the credit card, CSV, and expiration values before submitting
-    $form.find('#edit-submitted-payment-information-credit-card-number').val("****************");
-    $form.find('#edit-submitted-payment-information-cvc').val("****");
-    $form.find('#edit-submitted-payment-information-expiration-month').val("**");
-    $form.find('#edit-submitted-payment-information-expiration-year').val("**");
-    
-    // and submit
-    $form.get(0).submit();
-  }
-};
+      function stripeResponseHandler(status, response) {
+        var $form = jQuery('#webform-client-form-' + formID);
+
+        if (response.error) {
+          // Show the errors on the form
+          $form.find('.webform-component--error-markup').text(response.error.message);
+          $form.find('.form-submit').prop('disabled', false).css("background-color","").val("Make Payment");
+        } else {
+          // response contains id and card, which contains additional card details
+          var token = response.id;
+          // Insert the token into the form so it gets submitted to the server
+          $form.find('.webform-component--stripe-token input').val(token);
+          
+          // Remove the credit card, CSV, and expiration values before submitting
+          $form.find('#edit-submitted-payment-information-credit-card-number').val("****************");
+          $form.find('#edit-submitted-payment-information-cvc').val("****");
+          $form.find('#edit-submitted-payment-information-expiration-month').val("**");
+          $form.find('#edit-submitted-payment-information-expiration-year').val("**");
+          
+          // and submit
+          $form.get(0).submit();
+        }
+      };
+   }
+  };
+}(jQuery));
